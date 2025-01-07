d79159f0f07c279bee7d6d82fed00b85e93fbbe7
Nazar Hussain <nazarhussain@gmail.com>
2025-01-07 10:38:40 +0100

    Update the table output

--- src/utils/extendedTable.ts
+++ src/utils/extendedTable.ts
@@ -1,5 +1,5 @@
 import CliTable3, {TableConstructorOptions, Cell, CellOptions} from "cli-table3";
-export {CellValue, CellOptions} from "cli-table3";
+export type {CellValue, CellOptions} from "cli-table3";
 
 export class ExtendedTable extends CliTable3 {
   constructor(options?: TableConstructorOptions) {
