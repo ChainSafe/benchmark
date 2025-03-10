16c55529c35c26aea90af5daa8a906fc612ef393
Nazar Hussain <nazarhussain@gmail.com>
2025-03-10 11:46:32 +0100

    Add skipped tests

---
+++ test/perf/only_one_skipped.test.ts
@@ -0,0 +1,16 @@
+import {bench, describe} from "../../src/index.js";
+
+// This test file is to validate the error cases manually
+// should not be included into actual benchmarks as there are cases
+// in this file which will always fail.
+describe("only skipped tests", () => {
+  bench.skip("only skipped test in suite", () => {
+    const arr = Array.from({length: 10}, (_, i) => i);
+    arr.reduce((total, curr) => total + curr, 0);
+  });
+
+  bench.skip("only skipped test 2 in suite", () => {
+    const arr = Array.from({length: 10}, (_, i) => i);
+    arr.reduce((total, curr) => total + curr, 0);
+  });
+});
